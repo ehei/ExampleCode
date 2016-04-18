ecb45fe7270cd228f4470ff5e3c932e43a4cc860
ehei <ericheikkila@gmail.com>
2016-04-18 00:22:34 -0400

    Handling simple cases

--- src/com/ehei/convert/ArabicToRomanNumeralConverter.java
+++ src/com/ehei/convert/ArabicToRomanNumeralConverter.java
@@ -3,25 +3,59 @@ package com.ehei.convert;
 public class ArabicToRomanNumeralConverter {
 
 	private int valueToConvert;
+	private int workingValue;
+	private StringBuilder output = new StringBuilder();
 
 	public void setValue(int valueToConvert) {
 		
-		this.valueToConvert = valueToConvert;	
+		this.valueToConvert = valueToConvert;
+		this.workingValue = valueToConvert;
+		
+		this.output.setLength(0);
 	}
 
 	public boolean consume() {
 
-		return true;
+		if (this.workingValue > 0) {
+			
+			if (this.workingValue >= 10) {
+				
+				this.workingValue -= 10;
+				this.output.append("X");
+			}
+			else if (this.workingValue  >= 5) {
+				
+				this.workingValue -= 5;
+				this.output.append("V");
+			}
+			else if (this.workingValue  >= 1) {
+				
+				this.workingValue -= 1;
+				this.output.append("I");
+			}
+			
+			return true;
+		}
+		return false;
 	}
 
 	public boolean hasValue() {
 
-		return false;
+		return this.valueToConvert > 0;
 	}
 
 	public String getOutput() {
 		
-		return null;
+		return this.output.toString();
+	}
+
+	public String convert() {
+		
+		while (this.consume()) {
+			
+		}
+		
+		return this.output.toString();
 	}
 
 }
--- tests/com/ehei/convert/ArabicToRomanNumeralConverterTest.java
+++ tests/com/ehei/convert/ArabicToRomanNumeralConverterTest.java
@@ -60,6 +60,15 @@ public class ArabicToRomanNumeralConverterTest {
 	}
 
 	@Test
+	public void consume_givenAValueOfOne_shouldTakeOffOneFromTheTotal_andAddIToTheOutput() {
+
+		_converter.setValue(1);
+		
+		assertThat(_converter.consume()).isTrue();
+		assertThat(_converter.getOutput()).isEqualTo("I");
+	}
+
+	@Test
 	public void consume_givenAValueOfSix_shouldTakeOffFiveFromTheTotal_andAddVToTheOutput() {
 
 		_converter.setValue(6);
@@ -68,4 +77,28 @@ public class ArabicToRomanNumeralConverterTest {
 		assertThat(_converter.getOutput()).isEqualTo("V");
 	}
 
+	@Test
+	public void consume_givenAValueOfEleven_shouldTakeOffTenFromTheTotal_andAddXToTheOutput() {
+
+		_converter.setValue(11);
+		
+		assertThat(_converter.consume()).isTrue();
+		assertThat(_converter.getOutput()).isEqualTo("X");
+	}
+
+	@Test
+	public void convert_givenAValueOfEleven_shouldConsumeUntilReachingZero_andAddXIToOutput() throws Exception {
+		
+		_converter.setValue(11);
+		
+		assertThat(_converter.convert()).isEqualTo("XI");
+	}
+
+	@Test
+	public void convert_givenAValueOfThree_shouldConsumeUntilReachingZero_andAddIIIToOutput() throws Exception {
+		
+		_converter.setValue(3);
+		
+		assertThat(_converter.convert()).isEqualTo("III");
+	}
 }
